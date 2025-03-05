af844c6cf29fddb3d6d574658a8bc72bb57ad2c1
Glitchyyz <smnbanbury@gmail.com>
2025-03-05 13:33:04 -0500

    Add files via upload

--- Apimanager.js
+++ Apimanager.js
@@ -4,10 +4,17 @@ var List = {
     {
     }
 }
-function ApiCall() {
 
+function ApiCall() {
     const serverIp = document.getElementById("ServerInput").value; // gets ip from user
-    const apiUrl = `https://mcapi.us/server/status?ip=${serverIp}`;
+    var apiUrl = `https://api.mcsrvstat.us/3/${serverIp}`;
+    if(document.getElementById("toggle").checked ===true )
+        {
+            apiUrl = `https://api.mcsrvstat.us/bedrock/3/${serverIp}`
+        }
+
+
+
 
     
         fetch(apiUrl)
@@ -19,7 +26,7 @@ function ApiCall() {
         })
         .then(data => {
             console.log(data); 
-            if(data.server.name != null)
+            if(data.online === true)
             {
                 localStorage.removeItem("deeznutz6969");
                 
@@ -43,7 +50,7 @@ function ApiCall() {
                 div.style.top = "20px";
                 document.getElementById("ServerList").appendChild(div);
                 var image = document.createElement("img");
-                image.src = data.favicon
+                image.src = data.icon
                 image.style.borderRadius ="15px";
                 document.getElementById(idnumberer).appendChild(image);
                 var div1 = document.createElement("div");
@@ -51,18 +58,12 @@ function ApiCall() {
                 document.getElementById(idnumberer).appendChild(div1);
                 
                 var h3 = document.createElement("h3");
-                if(!Array.isArray(data.motd_json.extra))
-                    {
-                        h3.innerText ="";
-                        h3.innerText=data.motd_json;
-                    }else
-                    {
-                        h3.innerText = "motd not available"
-                    }
+                h3.innerHTML = data.motd.html;
+                h3.style = "font-size: 15px;font-family:Arial, Helvetica, sans-serif; background:black;"
                     document.getElementById(idnumberer).appendChild(h3);
                 var h2 = document.createElement("h2");
                 h2.style = "font-size: 15px;font-family:Arial, Helvetica, sans-serif; text-align: center;" ;
-                h2.innerText =  data.server.name+ "\n Players: " + data.players.now + "/" + data.players.max+"\n"+serverIp  ;
+                h2.innerText =  data.version+ "\n Players: " + data.players.online + "/" + data.players.max+"\n"+serverIp  ;
                 document.getElementById(idnumberer).appendChild(h2);
                 
                 idnumberer++;
@@ -81,6 +82,8 @@ function ApiCall() {
             console.error('Error:', error); // Log  errors
         });
     }
+
+
 function copyip(){
     const copyText = document.getElementById("ServerInput").value;
     navigator.clipboard.writeText(copyText);
