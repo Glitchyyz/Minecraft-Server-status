ca2f79c586441d003264c6275d70129f072e0c5a
Glitchyyz <smnbanbury@gmail.com>
2025-03-05 16:42:09 -0500

    Add files via upload

--- Apimanager.js
+++ Apimanager.js
@@ -4,8 +4,28 @@ var List = {
     {
     }
 }
-
+if(localStorage.getItem("deeznutz6969") !== null)
+{
+    const data = JSON.parse(localStorage.getItem("deeznutz6969"));
+    if (data.Server && typeof data.Server === "object") {
+        // Iterate over the keys of `data.Server`
+        for (let key in data.Server) {
+            if (data.Server.hasOwnProperty(key)) { // Ensure the key belongs to `data.Server`
+                document.getElementById("ServerInput").value = data.Server[key];
+                ApiCall();
+                List = data; 
+            }
+        }
+    } else {
+        console.error("data.Server is not an object or does not exist.");
+    }
+}
 function ApiCall() {
+    
+    var div2 =document.createElement("div");
+    div2.classList.add("loader");
+    div2.id = "loader";
+    document.getElementById("main").appendChild(div2)
     const serverIp = document.getElementById("ServerInput").value; // gets ip from user
     var apiUrl = `https://api.mcsrvstat.us/3/${serverIp}`;
     if(document.getElementById("toggle").checked ===true )
@@ -26,12 +46,14 @@ function ApiCall() {
         })
         .then(data => {
             console.log(data); 
+           
             if(data.online === true)
             {
+                
                 localStorage.removeItem("deeznutz6969");
                 
                 List.Server[idnumberer + 1] = serverIp;
-                localStorage.setItem("deeznutz6969",JSON.stringify( List))
+                localStorage.setItem("deeznutz6969",JSON.stringify( List));
                 
                 console.log(localStorage.getItem("deeznutz6969"));
 
@@ -59,7 +81,7 @@ function ApiCall() {
                 
                 var h3 = document.createElement("h3");
                 h3.innerHTML = data.motd.html;
-                h3.style = "font-size: 15px;font-family:Arial, Helvetica, sans-serif; background:black;"
+                h3.style = "font-size: 15px;font-family:Arial, Helvetica, sans-serif; background:grey;"
                     document.getElementById(idnumberer).appendChild(h3);
                 var h2 = document.createElement("h2");
                 h2.style = "font-size: 15px;font-family:Arial, Helvetica, sans-serif; text-align: center;" ;
@@ -67,20 +89,20 @@ function ApiCall() {
                 document.getElementById(idnumberer).appendChild(h2);
                 
                 idnumberer++;
-                 
+                document.getElementById("loader").remove();
             
             
             }else
             {
-                document.getElementById("Image").src = null;
-                document.getElementById("checkmark").innerText = ""
-                document.getElementById("ServerStuff").innerText = "Server Offline";
+                document.getElementById("loader").remove();
+
             }
 
         })
         .catch(error => {
             console.error('Error:', error); // Log  errors
         });
+        
     }
 
 
@@ -100,4 +122,5 @@ function Clear(){
                 moves.forEach((div )=> {
                     div.remove();
                         });
+        localStorage.setItem("deeznutz6969",JSON.stringify( List))
 }   
